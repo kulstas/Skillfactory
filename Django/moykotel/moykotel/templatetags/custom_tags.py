7bacdefac8253e4601940be80567136b45ec84dd
Stanislav <74294850+kulstas@users.noreply.github.com>
2023-10-07 14:48:54 +0300

    Delete Django/moykotel directory
    Transferring a directory to the root

--- Django/moykotel/moykotel/templatetags/custom_tags.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from django import template
-
-register = template.Library()
-
-# тег копирайта
-@register.simple_tag()
-def copyright(format_string='%Y'):
-   return f'© MoyKotel {datetime.utcnow().strftime(format_string)}'
