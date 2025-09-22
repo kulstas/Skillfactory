45e226a0d147ebc08ee2c2121010d1d8c78d3164
Stanislav Kulagin <kulstas@gmail.com>
2025-09-22 22:41:05 +0300

    Update Skillfactory tasks

--- training_tasks.py
+++ training_tasks.py
@@ -331,3 +331,20 @@
 # user_activity = {"user1": 10, "user2": 5, "user3": 20, "user4": 15, "user5": 10}
 # print(sort_users_by_activity(user_activity))
 # # ['user3', 'user4', 'user1', 'user5', 'user2']
+
+'''Задание 2.4 (External resource)'''
+# def fibonacci(n):
+#     if n <= 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fibonacci(n-1) + fibonacci(n-2)
+#
+# print(fibonacci(5))
+# #5
+# print(fibonacci(7))
+# #13
+# print(fibonacci(8))
+# #21
+
