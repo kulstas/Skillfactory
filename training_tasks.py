db47d8a9ebd34b33b7481ef71b6c88f32a9ee499
Stanislav Kulagin <kulstas@gmail.com>
2025-10-16 16:55:09 +0300

    Update Skillfactory tasks

--- training_tasks.py
+++ training_tasks.py
@@ -585,3 +585,33 @@
 # # Не получилось получить значение за 2 вызова
 # print(test_function())
 # # None
+
+
+'''Задание 7.6 (External resource)'''
+# import random
+#
+# def ensure_result_is_number(func):
+#     def wrapper(*args, **kwargs):
+#         res = func()
+#         if type(res) == int or type(res) == float:
+#             return res
+#         else:
+#             return None
+#
+#     return wrapper
+#
+# @ensure_result_is_number
+# def test_function():
+#     return random.choice(["Passed", 10, "Failed", 5.5])
+#
+# # Функция вернула не int и не float
+# print(test_function())
+# # None
+#
+# # Функция вернула float
+# print(test_function())
+# # 5.5
+#
+# # Функция вернула int
+# print(test_function())
+# # 10
