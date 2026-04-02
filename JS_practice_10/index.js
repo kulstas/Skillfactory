0876db6c04b7e6e39b275161cc42ee67682934d4
Stanislav <74294850+kulstas@users.noreply.github.com>
2026-04-02 18:00:41 +0300

    Solving final tasks

--- JS_practice_10/index.js
+++ JS_practice_10/index.js
@@ -45,3 +45,76 @@ if (userName && userAge && typeof userName == "string" && typeof userAge == "num
 } else {
         alert("Введите корректные данные!");
     }
+
+
+// Practice 6.1
+let userWord = "Довод";
+let clearWord = userWord.replace(/\s+/g, "").toLowerCase();
+let paliWord = "";
+
+for (let i = clearWord.length - 1; i >= 0; i -= 1) {
+  paliWord += clearWord.at(i);
+}
+
+if (clearWord === paliWord) {
+  console.log(`Слово «${userWord}» является палиндромом`);
+} else {
+  console.log(`Слово «${userWord}» НЕ является палиндромом`);
+}
+
+// Practice 6.2
+const arr = [1, 2, 3, 1, 5, 4, 2, 3, 5, 'they', 'don\'t', 'know', 'that', 'we', 'know', 'that', 'they', 'know' ];
+
+const uniqSet = new Set(arr);
+const uniqArr = Array.from(uniqSet);
+
+console.log(uniqArr);
+
+
+// Practice 6.3
+let userNum = prompt("Введите число");
++userNum;
+let outArr = [];
+
+if (!isNaN(userNum)) {
+  for (let i = 0; i <= userNum; i += 1) {
+    outArr.push(i);
+  }
+} else {
+  alert("Введено некорректное значение. Введите число.");
+}
+
+console.log(outArr);
+
+
+// Practice 6.4
+for (let i = 1; i <= 3; i += 1) {
+  if (i % 2 == 0) {
+    console.log("o x o");
+  } else {
+    console.log("x o x");
+  }
+}
+
+
+// Practice 6.5
+const obj = {
+  some: "some",
+  dom: "text",
+  arr: [1, 2, 3, 4, 5],
+  tom: "there",
+};
+
+let arrValues = [];
+
+for (let key in obj) {
+  if (Array.isArray(obj[key])) {
+    for (let value of obj[key]) {
+      arrValues.push(value);
+    }
+  } else {
+    arrValues.push(obj[key]);
+  }
+}
+
+console.log(arrValues);
